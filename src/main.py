7ad2ab4962cc99c288a4b9bcc4f51d3c48c8e6b7
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-10-14 23:57:53 -0500

    vocab

--- src/main.py
+++ src/main.py
@@ -4,3 +4,11 @@ import tensorflow_datasets as tfds
 import numpy as np
 
 dataset, info = tfds.load('tiny_shakespeare', with_info = True, as_supervised=False)
+
+# Getting all the texts from the dataset
+text = next(iter(dataset['train']))['text'].numpy().decode('utf-8')
+
+# Creating a vocab mapping from unique characters to indices
+vocab = sorted(set(text))
+char2idx = {char: idx for idx, char in enumerate(vocab)}
+idx2char = np.array(vocab)
