3b68d5a8aec0f81e35bbdf7803a031fc02122e3f
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-10-21 23:50:40 -0500

    vocab

--- src/main.py
+++ src/main.py
@@ -67,3 +67,20 @@ def loss(labels, logits):
     return tf.keras.losses.sparse_categorical_crossentropy(labels, logits, from_logits=True)
 
 model.compile(optimizer='adam', loss=loss)
+
+import os
+
+# directory where the checkpoints will be saved
+checkpoint_dir = './training_checkpoints'
+
+# name of the checkpoint files
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+
+checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+    filepath=checkpoint_prefix,
+    save_weights_only=True
+)
+
+# train the model
+EPOCHS = 10
+history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
