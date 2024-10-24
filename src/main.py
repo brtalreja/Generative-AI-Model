55b550f941e371dec4f10c05a5603c6f0dfa37ed
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-10-23 23:57:00 -0500

    function to generate text

--- src/main.py
+++ src/main.py
@@ -88,3 +88,25 @@ history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 model = build_model(vocab_size, embedding_dim, rnn_units, batch_size=1)
 model.load_weights(tf.train.latest_checkpoint(checkpoint_dir))
 model.build(tf.TensorShape([1, None]))
+
+def generate_text(model, start_string):
+    num_generate = 1000
+
+    input_eval = [char2idx[s] for s in start_string]
+    input_eval = tf.expand_dims(input_eval, 0)
+
+    text_generated = []
+
+    model.reset_states()
+    for i in range(num_generate):
+        predictions = model(input_eval)
+        predictions = tf.squeeze(predictions, 0)
+
+        predicted_id = tf.random.categorical(predictions, num_samples=1)[-1, 0].numpy()
+        input_eval = tf.expand_dims([predicted_id], 0)
+
+        text_generated.append(idx2char[predicted_id])
+
+    return (start_string + ''.join(text_generated))
+
+print(generate_text(model, start_string=u"QUEEN: So, lets end this"))
