099f857fd91230e1a6d1f6622441a353b31cf786
brtalreja <110854777+brtalreja@users.noreply.github.com>
2024-10-29 23:54:18 -0500

    Comments

--- src/main.py
+++ src/main.py
@@ -54,6 +54,8 @@ embedding_dim = 256
 # Number of RNN units
 rnn_units = 1024
 
+# RNN model with LSTM
+
 def build_model(vocab_size, embedding_dim, rnn_units, batch_size):
     model = tf.keras.Sequential([
         tf.keras.layers.Embedding(vocab_size, embedding_dim, batch_input_shape=[batch_size, None]),
